e179bd62fb23808970be044431269e8bc3e060d5
Teridium <zasdiry@gmail.com>
2023-01-30 23:09:16 +0400

    upd dish

--- sequence.py
+++ sequence.py
@@ -11,34 +11,31 @@ class sequence:
         f.close
         
     def get_command(self, command_step):
-        cmd_str = str(command_step)
-        return(self.data[cmd_str])
+        return(self.data[command_step])
     
     def go_next(self, command_step, result):
         if command_step is None: return
-        cmd_str = str(command_step)
         if result: 
-            if 'go_succ' in self.data[cmd_str].keys():
-                return(self.data[cmd_str]['go_succ'])
+            if 'go_succ' in self.data[command_step].keys():
+                return(self.data[command_step]['go_succ'])
             else:
                 return(command_step)
         else:
-            if 'go_fail' in self.data[cmd_str].keys():
-                return(self.data[cmd_str]['go_fail'])
+            if 'go_fail' in self.data[command_step].keys():
+                return(self.data[command_step]['go_fail'])
             else:
                 return(command_step)
         
     def get_msg(self, command_step, result):
         if command_step is None: return
-        cmd_str = str(command_step)
         if result: 
-            if 'msg_succ' in self.data[cmd_str].keys():
-                return(self.data[cmd_str]['msg_succ'])
+            if 'msg_succ' in self.data[command_step].keys():
+                return(self.data[command_step]['msg_succ'])
             else:
                 return('')
         else:
-            if 'msg_fail' in self.data[cmd_str].keys():
-                return(self.data[cmd_str]['msg_fail'])
+            if 'msg_fail' in self.data[command_step].keys():
+                return(self.data[command_step]['msg_fail'])
             else:
                 return('')
             
